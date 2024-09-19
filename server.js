8a9835a6076cd971fee6b1a0cab0e6f7dfe13bb5
koti <melamkotivenkateswarlu@gmail.com>
2024-09-19 11:16:06 +0530

    bcrypt added to package.json

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
+  origin: ["http://127.0.0.1:5174", "http://localhost:5174"],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
